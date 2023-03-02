a8ade17412d7d6eec961363b98035a78c63904e9
kreyNie <dworldxg@gmail.com>
2023-03-03 00:33:34 +0300

    Match fix in users.get function

--- helpfuncs/vkfunctions.py
+++ helpfuncs/vkfunctions.py
@@ -1,10 +1,9 @@
 from re import match
 from typing import Dict, List, Literal, Optional
 
+from config import api, ban_group_id, ban_reason_group_id
 from vkbottle import VKAPIError
 from vkbottle.tools import PhotoWallUploader
-
-from config import api, ban_group_id, ban_reason_group_id
 
 
 class VKHandler:
@@ -18,15 +17,22 @@ class VKHandler:
         name_case: Optional[Literal["nom", "gen", "dat", "acc", "ins", "abl"]] = None,
     ) -> Dict[str, str | int] | None:
         matched_mention = None
-        matched_link = match(r"vk\.com\/(.*)", username)
+        matched_link = match(r".*vk\.com\/(.*)", username)
         if matched_link is None:
             matched_mention = match(r"\[id(.+?)\|", username)
 
         if all(x is None for x in (matched_mention, matched_link)):
             return None
 
-        matched = matched_link if matched_link else matched_mention
-        info = await api.users.get([matched.group(1)], name_case)
+        if matched_link:
+            matched_link = matched_link.group(1)
+            if matched_link.startswith("id"):
+                matched = matched_link.strip("id")
+
+        if matched_mention:
+            matched = matched_mention.group(1)
+
+        info = await api.users.get(matched, name_case)
         if not info:
             return None
 
