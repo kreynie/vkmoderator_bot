1a4bb57b47c3b09af7999dd84c8a60e61c24d28f
kreyNie <kreynie@vk.com>
2023-10-08 03:37:11 +0300

    Deleted words detector due to lack of necessity

--- wordsdetector/__init__.py
+++
@@ -1,5 +0,0 @@
-from .ai import *
-from .handler import *
-from .states import *
-
-__all__ = ["BadWordsDetector", "AIState", "AIHandler"]
--- wordsdetector/states.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class AIState(Enum):
-    DISABLED = 0
-    ACTIVE = 1
