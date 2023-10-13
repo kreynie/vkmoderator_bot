1a08fee1d811958d4d4622ed1a80eed54dbd7c99
kreyNie <kreynie@vk.com>
2023-10-13 23:45:42 +0300

    Fixed missing mailing import

--- blueprints/__init__.py
+++ blueprints/__init__.py
@@ -4,7 +4,6 @@ from .help import help_labeler
 from .lead import lead_labeler
 from .legal import lt_labeler
 from .legal_extended import ltl_labeler
-from .mailing import mailing_labeler
 from .moderator import moderator_labeler
 from .moderator_extended import moderator_extended_labeler
 
@@ -14,7 +13,6 @@ labelers = [
     moderator_extended_labeler,
     moderator_labeler,
     help_labeler,
-    mailing_labeler,
     lt_labeler,
     ltl_labeler,
     common_labeler,
