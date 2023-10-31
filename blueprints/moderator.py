a4aa010bbc063398318c7a1623cb9badfdf6a47f
kreyNie <kreynie@vk.com>
2023-10-31 05:05:08 +0300

    Refactored ban and post system

--- blueprints/moderator.py
+++ blueprints/moderator.py
@@ -1,14 +1,14 @@
 from asyncio import sleep as asleep
 from time import localtime, strftime
+from typing import TypeAlias
 
-from vkbottle import VKAPIError
 from vkbottle.user import Message, UserLabeler
 
 from config import moderator_db
 from helpfuncs import VKHandler
 from helpfuncs.functions import ReformatHandler
 from utils.exceptions import handle_errors_decorator
-from utils.info_classes import UserInfo
+from utils.info_classes import BannerInfo, BanRegistrationInfo, UserInfo
 from .rules import CheckPermissions, Groups, Rights
 
 moderator_labeler = UserLabeler()
@@ -16,10 +16,12 @@ moderator_labeler.vbml_ignore_case = True
 moderator_labeler.custom_rules["access"] = CheckPermissions
 
 
+ReturnResult: TypeAlias = tuple[str | None, str | None]
+
+
 @moderator_labeler.private_message(
     access=[Groups.MODERATOR, Rights.LOW],
     text=[
-        "Бан <user> <comment> <ban_time> <banner_key>",
         "Бан <user> <comment> <ban_time>",
         "Бан <user> <comment>",
         "Бан <user>",
@@ -30,119 +32,117 @@ moderator_labeler.custom_rules["access"] = CheckPermissions
 async def ban(
     message: Message,
     user: str = "",
-    ban_time: str = "",
     comment: str = "",
-    banner_key: str = "",
+    ban_time: str = "",
 ) -> None:
     if not user:
-        await message.answer(
+        return await message.answer(
             'Нет нарушителя. Воспользуйся командой "Помощь" для справки'
         )
-        return
-    if not comment:
-        await message.answer("Забыл причину бана")
-        return
 
-    return_reason = None
+    if not comment:
+        return await message.answer("Забыл причину бана")
 
     user_info = await VKHandler.get_user_info(user)
-
     already_banned = await VKHandler.check_if_banned(user_info.id)
     if already_banned:
-        await message.answer("Пользователь уже забанен в группе")
-        return
+        return await message.answer("Пользователь уже забанен в группе")
 
-    reformatter = ReformatHandler(ban_time)
-    full_comment = await reformatter.comment(comment.lower())
-    ban_time_text = await reformatter.time_to_text()
-
-    if user_info is None:
-        return_reason = "Ошибка получения информации из ссылки"
-    if full_comment is None:
-        return_reason = 'Проверь ПРИЧИНУ бана (команда "сокращения")'
-    if ban_time_text is None:
-        return_reason = "Проверь ВРЕМЯ бана"
+    photo_attachments = message.get_photo_attachments()
+    ban_result, return_reason = await perform_ban(
+        banner_vk_id=message.from_id,
+        user_info=user_info,
+        ban_time=ban_time,
+        comment=comment.lower(),
+        attachments=photo_attachments,
+    )
 
     if return_reason is not None:
-        await message.answer(f"⚠️Ошибка получения информации\n{return_reason}")
-        return
+        return await message.answer(f"⚠️Ошибка получения информации\n{return_reason}")
 
-    photos = []
-    if (
-        ban_time_text.split()[-1] not in ("час", "сутки", "день")
-        or comment.lower() == "ода"
-    ):
-        photos = message.get_photo_attachments()
-        if not photos:
-            await message.answer(
-                "⚠️Ошибка получения информации\n \
-                    Проверь картинки для бани"
-            )
-            return
+    await message.answer(ban_result)
+    await asleep(3)
 
-    if banner_key.startswith("\\"):
-        banner_key = banner_key[1:]
-        banner = await moderator_db.get_user_by_id(int(banner_key.strip("МВ")))
-    else:
-        banner = await moderator_db.get_user_by_id(message.from_id)
-        level = await reformatter.moderator_id(banner.allowance, "МВ")
-        banner_key = level + str(banner.key)
 
-    time_unix = await reformatter.time()
-    try:
-        await VKHandler.ban(
-            owner_id=user_info.id,
-            end_date=time_unix,
-            reason=0,
-            comment=f"{full_comment} | {banner_key}",
-            comment_visible=True,
-        )
-    except Exception:
-        await message.answer("Ошибка при попытке бана")
-        raise
+async def perform_ban(
+    banner_vk_id: int,
+    user_info: UserInfo,
+    ban_time: str,
+    comment: str,
+    attachments: list | None = None,
+) -> ReturnResult:
+    reformatter = ReformatHandler(ban_time)
+    full_comment = reformatter.comment(comment)
+    ban_time_text = reformatter.time_to_text()
 
-    if time_unix is None:
-        ftime = "навсегда"
-    else:
-        ftime = f"\nБолеть будет до {strftime('%d.%m.%y %H:%M', localtime(time_unix))}"
+    if full_comment is None:
+        return None, 'Проверь ПРИЧИНУ бана (команда "сокращения")'
 
-    await message.answer(f"{user_info.full_name} получил банхаммером {ftime}\n")
+    if ban_time_text is None:
+        return None, "Проверь ВРЕМЯ бана"
 
-    if photos:
-        await post(
-            message=message,
-            photos=photos,
-            banner_id=banner.id,
-            banner_key=banner_key,
+    is_post_needed = ban_time_text not in ("час", "сутки", "день") or comment == "ода"
+    if is_post_needed and attachments is None:
+        return None, "Проверь картинки для бани"
+
+    banner = await get_banner_info(banner_vk_id)
+
+    time_unix = reformatter.time()
+
+    ftime = "навсегда"
+    if time_unix is not None:
+        ftime = f"Болеть будет до {strftime('%d.%m.%y %H:%M', localtime(time_unix))}"
+
+    result = await VKHandler.ban(
+        owner_id=user_info.id,
+        end_date=time_unix,
+        reason=0,
+        comment=f"{full_comment} | {banner.key}",
+        comment_visible=True,
+    )
+
+    if result != 1:
+        return None, "Не удалось забанить человека на стороне VK"
+
+    post_info = ""
+    if is_post_needed:
+        ban_info = BanRegistrationInfo(
+            banner_info=banner,
             user_info=user_info,
             comment=comment,
             ban_time=ban_time,
         )
+        post_info = await post(ban_info, attachments)
+        post_info = f"\n\n{post_info}"
 
-    await asleep(3)
+    return f"{user_info.full_name} получил банхаммером\n{ftime}{post_info}", None
+
+
+async def get_banner_info(user_vk_id: int) -> BannerInfo:
+    banner = await moderator_db.get_user_by_id(user_vk_id)
+    level = ReformatHandler.moderator_id(banner.allowance, "МВ")
+    banner_key = level + str(banner.key)
+    return BannerInfo(moderator=banner, key=banner_key)
 
 
 @handle_errors_decorator
 async def post(
-    message: Message,
+    ban_info: BanRegistrationInfo,
     photos: list,
-    banner_id: int,
-    banner_key: str,
-    user_info: UserInfo,
-    comment: str,
-    ban_time: str,
-) -> None:
+) -> str:
     uploaded_photos = await VKHandler.upload_images(photos)
     reply = (
-        f"{user_info.full_name}",
-        f"https://vk.com/id{user_info.id}",
-        f"{comment} - {ban_time}",
-        f"@id{banner_id}({banner_key})\n",
+        f"{ban_info.user_info.full_name}",
+        f"https://vk.com/id{ban_info.user_info.id}",
+        f"{ban_info.comment} - {ban_info.ban_time}",
+        f"@id{ban_info.banner_info.moderator.id}({ban_info.banner_info.key})",
     )
 
-    await VKHandler.post(
+    post_result = await VKHandler.post(
         from_group=True,
         message="\n".join(reply),
         attachments=uploaded_photos,
     )
-    await message.answer("Пост в бане успешно сделан")
+    if post_result is None:
+        return "❌ Не удалось сделать пост в бане"
+    return "✔ Пост в бане опубликован"
