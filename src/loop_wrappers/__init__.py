92cb609ed61e10418e511ec48c17eebaf279a3f0
kreynie <kreynie@vk.com>
2024-06-15 23:00:48 +0300

    added infinite online for bot's page

--- src/loop_wrappers/__init__.py
+++ src/loop_wrappers/__init__.py
@@ -4,8 +4,10 @@ __all__ = (
 )
 
 from .hot_issues_checkout import vk_loop_wrapper as hot_issues_checkout_wrapper
+from .infinite_online import vk_loop_wrapper as infinite_online_wrapper
 from .loop_wrapper import vk_loop_wrapper
 
 loop_wrappers = [
     hot_issues_checkout_wrapper,
+    infinite_online_wrapper,
 ]
---
+++ src/loop_wrappers/infinite_online.py
@@ -0,0 +1,9 @@
+from src.loop_wrappers import vk_loop_wrapper
+from src.vkbottle_config import vk_api
+from config import logger
+
+
+@vk_loop_wrapper.interval(minutes=5)
+async def update_account_online() -> None:
+    logger.debug(f"Updating account's online")
+    await vk_api.account.set_online(voip=False)
