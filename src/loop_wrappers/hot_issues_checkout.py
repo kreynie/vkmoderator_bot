73009f7c5d201d7d5ce4f84f60738d379d629cbe
kreynie <42591054+kreynie@users.noreply.github.com>
2025-09-27 22:45:46 +0300

    fix(project): Returned hot issues checkout

--- src/loop_wrappers/hot_issues_checkout.py
+++ src/loop_wrappers/hot_issues_checkout.py
@@ -30,7 +30,6 @@ def get_next_interval_for_hot_issues_checkout(
 
 @vk_loop_wrapper.interval(minutes=get_next_interval_for_hot_issues_checkout())
 async def check_hot_issues() -> None:
-    return
     new_issue = await check_for_new_hot_issues()
     if new_issue is None:
         return
